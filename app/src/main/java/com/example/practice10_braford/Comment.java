88682e6e713769e31238954d916744f0691b2c86
Julie Braford <jbraford@css.edu>
2020-04-01 10:52:27 -0500

    Initial commit, completed Practice 10 with additional changes

---
+++ app/src/main/java/com/example/practice10_braford/Comment.java
@@ -0,0 +1,34 @@
+package com.example.practice10_braford;
+
+public class Comment {
+    private long id;
+    private String comment;
+
+    public Comment(long id, String comment){
+        this.id = id;
+        this.comment = comment;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    // Will be used by the ArrayAdapter in the ListView
+    @Override
+    public String toString() {
+        return comment;
+    }
+}
+
